a97f0cf9d9ef2192323ff5aac2a5539f0f669ee0
satvik <satvik213161@gmail.com>
2021-07-16 10:28:12 +0530

    camera module working and saving videos

--- app/src/main/java/com/example/camerfun/CameraInterface.kt
+++ app/src/main/java/com/example/camerfun/CameraInterface.kt
@@ -1,43 +1,220 @@
 package com.example.camerfun
 
-import android.content.Intent
+import android.annotation.SuppressLint
+import android.content.Context
+import android.hardware.Camera
+import android.media.MediaRecorder
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
-import android.provider.MediaStore
+import android.os.Environment
+import android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_VIDEO
 import android.util.Log
 import android.view.View
+import android.widget.Button
+import android.widget.FrameLayout
 import android.widget.Toast
-import android.widget.VideoView
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.net.toFile
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+import java.net.URI
+import java.text.SimpleDateFormat
+import java.util.*
 
 class CameraInterface : AppCompatActivity() {
-    val REQUEST_VIDEO_CAPTURE = 1;
-    private var videoView:VideoView? = null;
+    private var mCamera: Camera? = null
+    private var mPreview: Preview? = null
+    private var mediaRecorder:MediaRecorder? = null
+    private var isRecording:Boolean = false
+    private var capture:Button? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_layout);
-        videoView = findViewById<VideoView>(R.id.video_view);
+
+        // Create an instance of Camera
+        mCamera = getCameraInstance();
+        capture = findViewById(R.id.button_capture)
+        
+        mCamera?.setDisplayOrientation(90);
+        mPreview = mCamera?.let {
+            // Create our Preview view
+            Preview(this, it)
+        }
+
+        // Set the Preview view as the content of our activity.
+        mPreview?.also {
+            val preview: FrameLayout = findViewById(R.id.camera_preview)
+            preview.addView(it)
+        }
+
+
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, intent: Intent?) {
-        super.onActivityResult(requestCode, resultCode, intent)
-        Log.d("WORKING@@@@@@1231",requestCode.toString());
+    override fun onDestroy() {
+        super.onDestroy();
+        //After executing the functionality
+        mCamera?.release();
+    }
 
-            if (requestCode == REQUEST_VIDEO_CAPTURE && resultCode == RESULT_OK) {
-                var videoUri: Uri? = intent?.data;
-                videoView?.setVideoURI(videoUri);
-                videoView?.start()
-            }
+
+    fun getCameraInstance(): Camera? {
+        return try {
+                Camera.open() // attempt to get a Camera instance
+        } catch (e: Exception) {
+            Log.d("ERROR_MESSAGE911",e.message.toString());
+            // Camera is not available (in use or does not exist)
+            null // returns null if camera is unavailable
+        }
     }
 
 
 
-    fun dispatchTakeVideoIntent(view: View) {
-        Intent(MediaStore.ACTION_VIDEO_CAPTURE).also { takeVideoIntent ->
-            takeVideoIntent.resolveActivity(packageManager)?.also {
-                startActivityForResult(takeVideoIntent, REQUEST_VIDEO_CAPTURE);
+    //MAIN CODE ----> CAPTURES VIDEO (Capture button function)
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun Capture(view: View){
+        try{
+            if (isRecording) {
+                // stop recording and release camera
+                mediaRecorder?.stop() // stop the recording
+                releaseMediaRecorder() // release the MediaRecorder object
+                mCamera?.lock() // take camera access back from MediaRecorder
+
+
+                //Save the media with URI
+                var uri:Uri = getOutputMediaFileUri(MEDIA_TYPE_VIDEO);
+                var file:File = uri.toFile()
+                var op:FileOutputStream = FileOutputStream(file)
+                op.write(file.toString().toByteArray())
+                op.close()
+
+
+
+                // inform the user that recording has stopped
+                setCaptureButtonText("Capture")
+                isRecording = false
+            } else {
+                // initialize video camera
+                if (prepareVideoRecorder()) {
+                    // Camera is available and unlocked, MediaRecorder is prepared,
+                    // now you can start recording
+                    mediaRecorder?.start()
+
+                    // inform the user that recording has started
+                    setCaptureButtonText("Stop")
+                    isRecording = true
+                } else {
+                    // prepare didn't work, release the camera
+                    releaseMediaRecorder()
+                    Toast.makeText(this,"Did not worked out",Toast.LENGTH_SHORT).show()
+                    // inform user
+                }
             }
+        }catch(e:Error){
+            e.message?.let { Log.d("ERROR6363333", it) }
+        }
+    }
+
+
+    private fun setCaptureButtonText(s: String) {
+        capture?.setText(s)
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun prepareVideoRecorder():Boolean {
+        mediaRecorder = MediaRecorder()
+
+        mCamera?.let { camera ->
+            // Step 1: Unlock and set camera to MediaRecorder
+            camera?.unlock()
+
+            mediaRecorder?.run {
+                setCamera(camera)
+
+                // Step 2: Set sources
+                setAudioSource(MediaRecorder.AudioSource.CAMCORDER)
+                setVideoSource(MediaRecorder.VideoSource.CAMERA)
+
+                // Step 4: Set output file
+                setOutputFile(getOutputMediaFile(MEDIA_TYPE_VIDEO).toString())
+
+
+                // Step 5: Set the preview output
+                setPreviewDisplay(mPreview?.holder?.surface)
+
+
+                    setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+                    setAudioEncoder(MediaRecorder.AudioEncoder.DEFAULT)
+                    setVideoEncoder(MediaRecorder.VideoEncoder.DEFAULT)
+
+
+                // Step 6: Prepare configured MediaRecorder
+                return try {
+                    prepare()
+                    true
+                } catch (e: IllegalStateException) {
+                    Log.d("TAG", "IllegalStateException preparing MediaRecorder: ${e.message}")
+                    releaseMediaRecorder()
+                    false
+                } catch (e: IOException) {
+                    Log.d("TAG", "IOException preparing MediaRecorder: ${e.message}")
+                    releaseMediaRecorder()
+                    false
+                }
+            }
+
+        }
+        return false
+    }
+
+    //Release the camera recorder resources
+    private fun releaseMediaRecorder() {
+        mediaRecorder?.reset() // clear recorder configuration
+        mediaRecorder?.release() // release the recorder object
+        mediaRecorder = null
+        mCamera?.lock() // lock camera for later use
+    }
+
+    /** Create a file Uri for saving an image or video */
+    private fun getOutputMediaFileUri(type: Int): Uri {
+        return Uri.fromFile(getOutputMediaFile(type))
+    }
+
+
+    //Checks the camera is free or not and proceed
+    /** Create a File for saving an image or video */
+    private fun getOutputMediaFile(type: Int): File? {
+
+        val mediaStorageDir = File(
+            Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM),
+            "CamerFun"
+        )
+
+        // Create the storage directory if it does not exist
+        mediaStorageDir.apply {
+            if (!exists()) {
+                if (!mkdirs()) {
+                    Log.d("CamerFun", "failed to create directory")
+                    return null
+                }
+            }
+        }
+
+        // Create a media file name
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
+        return when (type) {
+
+            MEDIA_TYPE_VIDEO -> {
+                File("${mediaStorageDir.path}${File.separator}VID_$timeStamp.mp4")
+            }
+            else -> null
         }
     }
 }
+
+
